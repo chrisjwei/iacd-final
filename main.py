d16db5a062311d911c2de5f873aea79a614e0c9e
Chris Wei <cwei1994@gmail.com>
2016-04-07 11:38:00 -0400

    made my own markov walker. dont need no broken libraries >_<

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import pysrt
 import string, os
-from pymarkovchain import MarkovChain
 
-def markov_convert(d):
-	return pykov.Chain(d);
+from random import random
+from bisect import bisect
 
-def dict_record(d, a, b):
-	key = (a,b)
+def weighted_choice(choices):
+    values, weights = zip(*choices.items())
+    total = 0
+    cum_weights = []
+    for w in weights:
+        total += w
+        cum_weights.append(total)
+    x = random() * total
+    i = bisect(cum_weights, x)
+    return values[i]
+
+def markov_walk(d, state, steps):
+	for i in xrange(steps):
+		print state
+		if (state in d):
+			state_transitions = d[state]
+			next_state = weighted_choice(state_transitions)
+			state = next_state
+		else:
+			return
+		
+		
+
+def dict_record(d, key, val):
+	if (key == ""): return
 	if (key in d):
-		d[key] += 1
+		chain = d[key]
+		if (val in chain):
+			chain[val] += 1
+		else:
+			chain[val] = 1
 	else:
-		d[key] = 1
+		chain = {val: 1}
+	d[key] = chain
 	
 def sub_format(txt):
 	return txt.encode('utf-8') \
@@ -18,21 +45,24 @@ def sub_format(txt):
               .replace('\n', ' ') \
               .lstrip()
 
-def subs_parse(subs):
-	parsed_string = ""
+def subs_parse(subs, d):
+	if len(subs) == 0:
+		return
+	prev_word = ""
 	for sub in subs:
-		parsed_string = parsed_string + " " + sub_format(sub.text)
-	return parsed_string
+		formatted = sub_format(sub.text)
+		words = formatted.split()
+		if len(words) == 0: continue
+		for word in words:
+			dict_record(d, prev_word, word)
+			prev_word = word
 
 def main():
-	mc = MarkovChain()
-	training_str = ""	
+	d = {}
 	for filename in os.listdir("subs"):
 		subs = pysrt.open('subs/'+filename)
-		sub_str = subs_parse(subs)
-		training_str = training_str + " " + sub_str
-	mc.generateDatabase(training_str)
-	mc.generateString(10)
+		subs_parse(subs, d)
+	markov_walk(d, "Anakin", 15)
 
 if __name__ == "__main__":
 	main()	
