1a716842cd0dc3393ad8d381af36b2bf99848dc5
Chris Wei <cwei1994@gmail.com>
2016-04-19 11:35:29 -0400

    moved to subtitle level markov

--- main.py
+++ main.py
@@ -1,76 +1,108 @@
 import pysrt
 import string, os
-
-from random import random
+import random, re
 from bisect import bisect
 
-def weighted_choice(choices):
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
+
+def subFormat(txt):
+	return remove_tags(txt).encode('utf-8') \
+			  .translate(None, string.punctuation) \
+              .replace('\n', ' ') \
+              .lstrip()
+
+class Subtitle:
+	def __init__(self, sub, filename):
+		self.timeStart = timeToSeconds(sub.start)
+		self.timeEnd = timeToSeconds(sub.end)
+		self.filename = filename
+		self.text = subFormat(sub.text)
+		self.words = self.text.split()
+		self.key = self.words[0]
+
+	def getKey(self):
+		return self.key
+
+	def getLastWords(self, order):
+		return " ".join(self.words[-order:])
+	def __str__(self):
+		return self.filename + " (" + str(self.timeStart) + "," + \
+			   str(self.timeEnd) + ") \"" + self.text + "\""
+
+def timeToSeconds(timeObj):
+	return timeObj.seconds + 60*(timeObj.minutes) + 60*(timeObj.hours)
+
+def weightedChoice(choices):
     values, weights = zip(*choices.items())
     total = 0
     cum_weights = []
     for w in weights:
         total += w
         cum_weights.append(total)
-    x = random() * total
+    x = random.random() * total
     i = bisect(cum_weights, x)
     return values[i]
 
-def markov_walk(d, state, steps, order):
-	for i in xrange(steps):
-		if (state in d):
-			print state
-			state_transitions = d[state]
-			next_state = weighted_choice(state_transitions)
-			words = state.split()
-			words.pop(0)
-			words.append(next_state)
-			state = " ".join(words)
-		else:
-			return
-		
-		
-
-def dict_record(d, words, val):
-	for word in words:
-		if (word == ""): return
-	# convert array of words into string
-	sentence = " ".join(words)
-	if (sentence in d):
-		chain = d[sentence]
-		if (val in chain):
-			chain[val] += 1
-		else:
-			chain[val] = 1
-	else:
-		chain = {val: 1}
-	d[sentence] = chain
+def randomChoice(choices):
+	return random.choice(choices)
 	
-def sub_format(txt):
-	return txt.encode('utf-8') \
-			  .translate(None, string.punctuation) \
-              .replace('\n', ' ') \
-              .lstrip()
-
-def subs_parse(subs, d, order):
-	if len(subs) == 0:
-		return
-	prev_words = [""]*order
-	for sub in subs:
-		formatted = sub_format(sub.text)
-		words = formatted.split()
-		if len(words) == 0: continue
-		for word in words:
-			dict_record(d, prev_words, word)
-			prev_words.pop(0)
-			prev_words.append(word)
 
 def main():
+	# dictionary of all subtitles, keyed by their first word
 	d = {}
-	order = 3
+	order = 2
+	subOrder = []
 	for filename in os.listdir("subs"):
 		subs = pysrt.open('subs/'+filename)
-		subs_parse(subs, d, order)
-	markov_walk(d, "You have only", 100, order)
+		for sub in subs:
+			CurrentSubtitle = Subtitle(sub, filename)
+			key = CurrentSubtitle.getKey()
+			#print "<" + key,
+			#print CurrentSubtitle.getLastWords(order) + ">",
+			#print CurrentSubtitle.text
+			# if the current starting word exists in the dictionary,
+			# add an additional subtitle object to the list of already
+			# existing subtitles that start with that word
+			if (key in d):
+				d[key].append(CurrentSubtitle)
+			else:
+				d[key] = [CurrentSubtitle]
+			# record subtitle in order
+			subOrder.append(CurrentSubtitle)
+	
+	# now we want to construct a markov chain based on the last n words
+	# of a subtitle, and the most likely next word it should transition to
+	PrevSubtitle = None
+	# an element in markov "sentence": {"word1": count1, "word2": count2}
+	markov = {}
+	for CurrentSubtitle in subOrder:
+		if PrevSubtitle != None:
+			firstWord = CurrentSubtitle.getKey()
+			lastWords = PrevSubtitle.getLastWords(order)
+			if lastWords in markov:
+				if firstWord in markov[lastWords]:
+					markov[lastWords][firstWord] += 1
+				else:
+					markov[lastWords][firstWord] = 1
+			else:
+				markov[lastWords] = {firstWord: 1}
+		PrevSubtitle = CurrentSubtitle
+	
+	CurrentSubtitle = subOrder[0]
+	for i in xrange(100):
+		print(CurrentSubtitle)
+		lastWords = CurrentSubtitle.getLastWords(order)
+		transitions = markov[lastWords]
+		# get weighted choice
+		nextWord = weightedChoice(transitions)
+		possibleSubtitles = d[nextWord]
+		CurrentSubtitle = randomChoice(possibleSubtitles)
+		
+			
+				
 
 if __name__ == "__main__":
 	main()	
