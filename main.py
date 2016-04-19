0e03ba7179d79cf200fac884f5c3ffca2019d229
Chris Wei <cwei1994@gmail.com>
2016-04-19 10:00:46 -0400

    multi order markov

--- main.py
+++ main.py
@@ -15,29 +15,35 @@ def weighted_choice(choices):
     i = bisect(cum_weights, x)
     return values[i]
 
-def markov_walk(d, state, steps):
+def markov_walk(d, state, steps, order):
 	for i in xrange(steps):
-		print state
 		if (state in d):
+			print state
 			state_transitions = d[state]
 			next_state = weighted_choice(state_transitions)
-			state = next_state
+			words = state.split()
+			words.pop(0)
+			words.append(next_state)
+			state = " ".join(words)
 		else:
 			return
 		
 		
 
-def dict_record(d, key, val):
-	if (key == ""): return
-	if (key in d):
-		chain = d[key]
+def dict_record(d, words, val):
+	for word in words:
+		if (word == ""): return
+	# convert array of words into string
+	sentence = " ".join(words)
+	if (sentence in d):
+		chain = d[sentence]
 		if (val in chain):
 			chain[val] += 1
 		else:
 			chain[val] = 1
 	else:
 		chain = {val: 1}
-	d[key] = chain
+	d[sentence] = chain
 	
 def sub_format(txt):
 	return txt.encode('utf-8') \
@@ -45,24 +51,26 @@ def sub_format(txt):
               .replace('\n', ' ') \
               .lstrip()
 
-def subs_parse(subs, d):
+def subs_parse(subs, d, order):
 	if len(subs) == 0:
 		return
-	prev_word = ""
+	prev_words = [""]*order
 	for sub in subs:
 		formatted = sub_format(sub.text)
 		words = formatted.split()
 		if len(words) == 0: continue
 		for word in words:
-			dict_record(d, prev_word, word)
-			prev_word = word
+			dict_record(d, prev_words, word)
+			prev_words.pop(0)
+			prev_words.append(word)
 
 def main():
 	d = {}
+	order = 3
 	for filename in os.listdir("subs"):
 		subs = pysrt.open('subs/'+filename)
-		subs_parse(subs, d)
-	markov_walk(d, "Anakin", 15)
+		subs_parse(subs, d, order)
+	markov_walk(d, "You have only", 100, order)
 
 if __name__ == "__main__":
 	main()	
