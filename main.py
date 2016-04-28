e4a6837fe93de22a8570d1a1cba64f39bff5aea8
Chris Wei <cwei1994@gmail.com>
2016-04-28 16:42:38 -0400

    rhyming scheme

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pysrt, nltk
+from nltk.corpus import cmudict
 import string, os, csv, random, re, glob, pickle, gc, math, subprocess
 from bisect import bisect
 import moviepy.editor as mpy
+import moviepy.video.fx.all as mpyfx
+import numpy as np
 from enum import Enum
 from collections import Counter
 
@@ -13,53 +16,19 @@ def remove_tags(text):
     return TAG_RE.sub('', text)
 
 def subFormat(txt):
-	return remove_tags(txt).encode('utf-8') \
+	return remove_tags(txt).encode('ascii', 'ignore') \
               .replace('\n', ' ') \
               .lstrip()
-
-class PhraseType(Enum):
-	unknown = -1
-	statement = 0
-	shortQuestion = 1
-	longQuestion = 2
-	response = 3
-	yes = 4
-	no = 5
-	maybe = 6
+	
 
 
 class Subtitle:
-	def __extractSubtype(self, sentence):
-		# REMOVE THIS MAYBE?
-		return PhraseType.unknown
-
-		try:
-			tokens = nltk.word_tokenize(sentence)
-			tagged = nltk.pos_tag(tokens)
-		except:
-			print "failed on sentence: " + sentence
-			return PhraseType.unknown
-		
-		if (sentence[-1] == '?'):
-			if (len(tokens) < 6):
-				return PhraseType.shortQuestion
-			else:
-				return PhraseType.longQuestion
-		[words, tags] = zip(*tagged)
-		if ('UH' in tags):
-			interjection = words[tags.index('UH')].lower()
-			if (interjection == 'yes' or interjection == 'yeah'):
-				if (len(tokens) < 4):
-					return PhraseType.yes
-				else:
-					return PhraseType.unknown
-			if (interjection == 'no'):
-				return PhraseType.no
-
-
-		return PhraseType.unknown
+	
+	_CMUDICT = cmudict.dict()
 
 	def __init__(self, sub, filename):
+		self.start = sub.start
+		self.end = sub.end
 		self.timeStart = timeToSeconds(sub.start)
 		self.timeEnd = timeToSeconds(sub.end)
 
@@ -68,18 +37,78 @@ class Subtitle:
 		
 		self.text = subFormat(sub.text)
 		self.textWordsOnly = ''.join(ch for ch in self.text if ch not in PUNC_EXCLUDE)
-		self.split = self.text.split()
-		self.words = self.textWordsOnly.split()
-		self.counter = Counter(self.words)
-		self.phraseType = self.__extractSubtype(self.text)
+		self.split = nltk.word_tokenize(self.text)
+		self.words = nltk.word_tokenize(self.textWordsOnly)
+		self.lastWord = self.words[-1]
+		# extract pronunciation data
+		vocalData = []
+		for word in self.words:
+			word = word.lower()
+			if word in Subtitle._CMUDICT:
+				vocalData.append(Subtitle._CMUDICT[word][0])
+			else:
+				vocalData = []
+				break
+		# if all went well in translating words to pronunciation, extract the
+		# number of syllables
+		if len(vocalData) > 0:
+			self.numSyllable = 0
+			for word in vocalData:
+				# for each word in the vocal data, count the number of stress
+				# characters to determine the number syllables
+				self.numSyllable += len(list(y for y in word if y[-1].isdigit()))
+			self.lastWordData = vocalData[-1]
+		else:
+			self.numSyllable = -1
+			self.lastWordData = []
+		
 
 	def __str__(self):
 		return self.filename + " (" + str(self.timeStart) + "," + \
 			   str(self.timeEnd) + ") \"" + self.text + "\""
 
-	def getWordFrequency(self):
-		return self.counter
+	def isRhyme(self, TargetSubtitle):
+		p1 = self.lastWordData
+		p2 = TargetSubtitle.lastWordData
+		w1 = self.lastWord
+		w2 = TargetSubtitle.lastWord
+		if p1 == p2: return False
 
+		p1Accents = list((idx, val, int(val[-1])) for (idx, val) in enumerate(p1) if val[-1].isdigit())
+		p2Accents = list((idx, val, int(val[-1])) for (idx, val) in enumerate(p2) if val[-1].isdigit())
+		
+		# cannot rhyme without syllables
+		if (len(p1Accents) == 0 or len(p2Accents) == 0):
+			return False
+
+		p1AccentOrderOnly = map(lambda (idx, val, order): order, p1Accents)
+		p1MaxAccent = max(p1AccentOrderOnly)
+		p1AccentIndex = p1AccentOrderOnly.index(p1MaxAccent)
+
+		p2AccentOrderOnly = map(lambda (idx, val, order): order, p2Accents)
+		p2MaxAccent = max(p2AccentOrderOnly)
+		p2AccentIndex = p2AccentOrderOnly.index(p2MaxAccent)
+
+		if p1Accents[p1AccentIndex:] != p2Accents[p2AccentIndex:]:
+			return False
+		
+		if p1[-2:] == p2[-2:]:
+			#print (w1, p1Accents, w2, p2Accents)
+			return True
+		return False
+
+	def getRhymingSubtitles(self, possibleSubtitles):
+		rhymingSubtitles = []
+		for CurrentSubtitle in possibleSubtitles:
+			# Can't rhyme with yourself lol
+			if CurrentSubtitle == self:
+				continue
+			if self.isRhyme(CurrentSubtitle):
+				rhymingSubtitles.append(CurrentSubtitle)
+		return rhymingSubtitles				
+
+
+	
 
 def timeToSeconds(timeObj):
 	return timeObj.seconds + 60*(timeObj.minutes) + 3600*(timeObj.hours) + timeObj.milliseconds/1000.0
@@ -100,13 +129,30 @@ def randomChoice(choices):
 
 def parseSubs(subtitles):
 	for filename in os.listdir("subs"):
-		subtitles[filename] = []
 		print ("Parsing srt file: " + filename)
-		subs = pysrt.open('subs/'+filename)
-		for sub in subs:
+		try:
+			subs = pysrt.open('subs/'+filename)
+		except:
+			print "Could not parse "+ filename
+			continue
+		for i in xrange(len(subs)):
+			sub = subs[i]
+			if i != len(subs)-1:
+				# some subbers are crazy impatient! subs drop out prematurely
+				# given a threshold for about 1.5 seconds, we will extend the sub up to
+				# 750ms based on the start tiem of the next subtitle
+				nextSub = subs[i+1]
+				timeToNextSub = nextSub.start - sub.end
+				secondsToNextSub = timeToNextSub.seconds + timeToNextSub.milliseconds/1000.0
+				if secondsToNextSub <= 2:
+					sub.end.seconds += secondsToNextSub/2.0
+				else:
+					sub.end.seconds += 1
+
+
 			if (len(sub.text.split()) == 0): continue
 			CurrentSubtitle = Subtitle(sub, filename)
-			subtitles[filename].append(CurrentSubtitle)
+			subtitles.append(CurrentSubtitle)
 
 def getFilename(filenameBase):
 	path = "movies/" + filenameBase + ".*"
@@ -116,26 +162,35 @@ def getFilename(filenameBase):
 	else:
 		return ""
 
-def getForcedAlignClip(Subtitle, movieFilename, selectedWord):
-	original_clip = mpy.VideoFileClip(movieFilename).subclip(Subtitle.timeStart,Subtitle.timeEnd)
-	'''text = Subtitle.textWordsOnly
-	with open("temp/temp.txt", "wb") as textFile:
-		textFile.write(text)
-	original_clip.audio.write_audiofile("temp/temp.wav")
-	subprocess.call(["python","./penn_aligner/align.py", "./temp/temp.wav", "./temp/temp.txt", "./temp/aligned.textgrid"], shell=True)
-	'''
-	return original_clip
+def appendSubtitle(subs, Subtitle):
+	newSub = pysrt.srtitem.SubRipItem()
+	newSub.start = pysrt.srttime.SubRipTime()
+	newSub.end = pysrt.srttime.SubRipTime()
+	if (len(subs) == 0):
+		newSub.start.milliseconds = 0
+	else:
+		newSub.start = subs[-1].end
+	newSub.end = Subtitle.end - Subtitle.start + newSub.start
+	newSub.text = Subtitle.text
+	newSub.index = len(subs)+1
 
+	subs.append(newSub)
 
-
-def writeClips(selectedSubtitles, outputBase, selectedWord):
+def writeClips(selectedSubtitles, outputBase):
 	clips = []
 	count = 0
 	superclips = 0
+	volumeFn = lambda array: np.sqrt(((1.0*array)**2).mean())
+	desiredVolume = 0.03
+	# create subtitle file to go along with it
+	newSubs = pysrt.srtfile.SubRipFile()
 	for CurrentSubtitle in selectedSubtitles:
 		print CurrentSubtitle
+		appendSubtitle(newSubs, CurrentSubtitle)
 		movieFilename = getFilename(CurrentSubtitle.filename)
-		clip = getForcedAlignClip(CurrentSubtitle, movieFilename, selectedWord)
+		clip = mpy.VideoFileClip(movieFilename).subclip(CurrentSubtitle.timeStart,CurrentSubtitle.timeEnd)
+		volume = volumeFn(clip.audio.to_soundarray())
+		clip.audio = clip.audio.fx(mpy.afx.volumex, desiredVolume/volume)
 		clips.append(clip)
 		count += 1
 		if (count == 50):
@@ -146,6 +201,11 @@ def writeClips(selectedSubtitles, outputBase, selectedWord):
 			gc.collect()
 			count = 0
 			superclips += 1
+	
+	# calculate the lowest resolution height
+	minHeight = min(map(lambda clip: clip.h, clips))
+	for (i, clip) in enumerate(clips):
+		clips[i] = mpyfx.resize(clip, width=clip.w * minHeight / clip.h , height=minHeight)
 
 	print "Concatenating clips"
 	superClip = mpy.concatenate_videoclips(clips, method="compose")
@@ -157,93 +217,50 @@ def writeClips(selectedSubtitles, outputBase, selectedWord):
 	superClip = None
 	clips = []
 	gc.collect()
-
-
-def extractAndWriteClips(subtitles, word):
-	selectedSubtitles = []
-	for Subtitle in subtitles:
-		if (Subtitle.getWordFrequency()[word] > 0):
-			selectedSubtitles.append(Subtitle)
-	writeClips(selectedSubtitles, subtitles[0].filename + "_" + word, word)
+	print "Writing srt file"
+	newSubs.save(outputBase + ".srt")
 
 def main():
-	movieSubtitles = {} # dictionary of movie to subtitle mappings
-	n = 10
-	if (os.path.isfile('data.pickle')):
-		print "Found pickle file, using instead"
-		with open('data.pickle', 'rb') as f:
-			(movieSubtitles, movieFrequencies, totalFrequencies) = pickle.load(f)
+	if os.path.isfile("rhyme.pickle"):
+		with open('rhyme.pickle','rb') as f:
+			rhymes = pickle.load(f)
 	else:
-		print "Parsing movie subtitles"
-		parseSubs(movieSubtitles)
-		# get the global counter for all movies
-		print "Getting global word frequency"
-		totalFrequencies = Counter()
-		movieFrequencies = {}
-		for movie in movieSubtitles:
-			print "Parsing " + str(movie)
-			subtitles = movieSubtitles[movie]
-			currentMovieFrequencies = Counter()
-			for subtitle in subtitles:
-				currentMovieFrequencies += subtitle.getWordFrequency()
-			for word in currentMovieFrequencies:
-				totalFrequencies[word] += 1
-			movieFrequencies[movie] = currentMovieFrequencies
-		print "Pickling movie subtitles"
-		with open('data.pickle', 'wb') as f:
-			pickle.dump((movieSubtitles, movieFrequencies, totalFrequencies), f, pickle.HIGHEST_PROTOCOL)
+		allSubtitles = []
+		parseSubs(allSubtitles)
+		numSyllableSegmentedSubtitleList = []
+		for i in xrange(8,13):
+			Subtitles = filter(lambda Subtitle: Subtitle.numSyllable == i, allSubtitles)
+			numSyllableSegmentedSubtitleList.append(Subtitles)
+			print ("Found " + str(len(Subtitles)) + " subtitles with " + str(i) + " syllables")
+		rhymes = []
+		# keep looking for a subtitle with a rhyme
+		for sameNumSyllableSubtitles in numSyllableSegmentedSubtitleList:
+			for CurrentSubtitle in sameNumSyllableSubtitles:
+				rhymingSubtitles = CurrentSubtitle.getRhymingSubtitles(sameNumSyllableSubtitles)
+				if len(rhymingSubtitles) > 0:
+					rhymes.append((CurrentSubtitle, rhymingSubtitles))
+		print "Found " + str(len(rhymes)) + " rhyming subtitles"
+		with open('rhyme.pickle', 'wb') as f:
+			pickle.dump(rhymes, f, pickle.HIGHEST_PROTOCOL)
 
-
-	for movie in movieFrequencies:
-		relativeFrequencies = {}
-		currentMovieFrequencies = movieFrequencies[movie]
-		(maxWord, maxWordFreq) = currentMovieFrequencies.most_common(1)[0]
-		for word in currentMovieFrequencies:
-			currentWordFreq = currentMovieFrequencies[word]
-			tf = 0.5 + 0.5 * (currentWordFreq/(maxWordFreq + 0.0))
-			idf = math.log(1 + (len(movieFrequencies.keys()) / (totalFrequencies[word] + 0.0)))
-			tfidf = tf*idf
-			relativeFrequencies[word] = tfidf
-		counter = Counter(relativeFrequencies)
-		#mostCommonWords, mostCommonWordsTfidf = zip(*counter.most_common(50))
-		# remove all words with 
-		filtered = \
-				filter(\
-				lambda (word, relfreq): currentMovieFrequencies[word] > 10
-				, counter.most_common(100))
-		if (len(filtered) == 0):
-			print "No interesting words found"
-			continue
-		mostCommonWords, mostCommonWordsTfidf = zip(*filtered)
-		if __USER_CHOICE:
-			i = 0
-			for choice in mostCommonWords:
-				print "(" + str(i) + ")" + choice + "[" + str(currentMovieFrequencies[choice]) + "]"
-				i += 1
-			i = input("Select a word to use or -1 to skip: ")
-			if (i == -1):
-				continue
-			selectedWord = mostCommonWords[i]
-		else:
-			selectedWord = random.choice(mostCommonWords)
-		print "rendering " + movie + " using the word " + selectedWord + " with frequency of " + str(currentMovieFrequencies[selectedWord])
-		extractAndWriteClips(movieSubtitles[movie], selectedWord)
-		
-
-	'''
+	# in order is boring
+	random.shuffle(rhymes)
 	selectedSubtitles = []
-	#questionSubtitles = filter(lambda s: s.phraseType == PhraseType.shortQuestion, subtitles)
-	yesSubtitles = filter(lambda s: s.phraseType == PhraseType.yes, subtitles)
-	#noSubtitles = filter(lambda s: s.phraseType == PhraseType.no, subtitles)
+	for (CurrentSubtitle, rhymingSubtitles) in rhymes:
+		flag = False
+		for i in xrange(10):
+			selected = random.choice(rhymingSubtitles)
+			if selected not in selectedSubtitles:
+				flag = True
+				break;
+		if (flag):
+			selectedSubtitles.append(CurrentSubtitle)
+			selectedSubtitles.append(selected)
+	
+	writeClips(selectedSubtitles[0:10], "final")
 
-	#for i in xrange(20):
-		#selectedSubtitles.append(random.choice(questionSubtitles))
-		#yes = random.choice(yesSubtitles)
-		#no = random.choice(noSubtitles)
-		#selectedSubtitles.append(random.choice(yesSubtitles))
-	random.shuffle(yesSubtitles)
-	writeClips(yesSubtitles)
-	'''
+
+
 
 if __name__ == "__main__":
 	main()	
